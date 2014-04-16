09e29b6f10d5a0510617fa3b7e1e9f5d834be1ad
michael zak <michael@lnx-michaelz.(none)>
2014-04-16 10:39:03 +0300

    Uploading all the files.

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-battery_status
+Battery Status
 ==============
 
-A simple battery status inidcator written in LUA using GTK.
+A simple battery status inidcator written in LUA using GTK to use with window managers that don't provide one, such as Awesome.
+
+Usage
+=====
+You will need to install LGI with luarocks with the command
+luarocks install lgi
+Or get it from https://github.com/pavouk/lgi
+
+Afterwards just run `lua /path/to/status.lua` in your initialization script, and enjoy!
---
+++ status.lua
@@ -0,0 +1,80 @@
+
+local lgi = require 'lgi'
+local Gtk = lgi.require('Gtk')
+local GLib = lgi.GLib
+local rex = require("rex_posix")
+
+local statusIcon = Gtk.StatusIcon {
+    visible = true,
+    tooltip_text = "I'm not sure",
+    file = "./battery-icons/100.png"
+}
+
+local function get_acpi_res()
+  local acpi = io.popen('acpi')
+  local acpi_res = acpi:read("*line")
+  acpi:close()
+  return acpi_res
+end
+
+local function get_icons_dir()
+  local running_script = debug.getinfo(get_icons_dir).short_src
+  print(running_script)
+  local dir = rex.match(running_script, [[^(.*\/)?[^\/]+.lua]]) or "./"
+  return dir .. "battery-icons/"
+end
+
+local icons_dir = get_icons_dir()
+local function get_battery_file(percent)
+  local img_name = (percent > 90 and "100") or
+                   (percent > 70 and "80") or
+                   (percent > 50 and "60") or
+                   (percent > 30 and "40") or
+                   (percent > 10 and "20") or
+                   "0"
+
+  return string.format("%s%s.png", icons_dir, img_name)
+end
+
+local alert_thresholds = {}
+alert_thresholds[20] = false
+alert_thresholds[10] = false
+alert_thresholds[5] = false
+local function alert_low_battery(percent)
+  for threashold, was_raised in pairs(alert_thresholds) do
+    if percent < threashold then
+      if not was_raised then
+        local dialog = Gtk.MessageDialog {
+          message_type = 'ERROR', buttons = 'CLOSE',
+          text = ("Battery low! Have only %s percent!"):format(percent),
+          on_response = Gtk.Widget.destroy
+        }
+        dialog:show_all()
+
+        for other_threashold, other_raised in pairs(alert_thresholds) do
+          if other_threashold >= threashold then
+            alert_thresholds[other_threashold] = true
+          end
+        end
+      end
+    else
+      was_raised = false
+    end
+  end
+end
+
+local battery_rex = rex.new([[([^,]{1,3})%]])
+local function check_battery()
+  local acpi_res = get_acpi_res()
+
+  statusIcon.tooltip_text = acpi_res
+  local percent = tonumber(battery_rex:match(acpi_res))
+  statusIcon.file = get_battery_file(percent)
+  alert_low_battery(percent)
+
+  return true
+end
+
+check_battery()
+GLib.timeout_add(GLib.PRIORITY_DEFAULT, 1 * 1000 ,check_battery)
+Gtk.main()            -- make it alive, waiting for events
